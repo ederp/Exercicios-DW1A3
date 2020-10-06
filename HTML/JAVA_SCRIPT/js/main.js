6af3acbb2d86d60e7f2723b3ec75bf946b562316
AdrianoSuares <61994392+AdrianoSuares@users.noreply.github.com>
2020-10-05 22:01:16 -0300

    Update main.js

--- HTML/JAVA_SCRIPT/js/main.js
+++ HTML/JAVA_SCRIPT/js/main.js
@@ -2,8 +2,9 @@
 function soma(){
   var a = parseInt(document.getElementById('a').value);
   var b = parseInt(document.getElementById('b').value);
-  var total = document.getElementById('total');
+  var totalSoma = document.getElementById('totalSoma');
+  var total;
   total = a + b;
 
-  total.innerHTML =total;
+  totalSoma.innerHTML=total;
 }
