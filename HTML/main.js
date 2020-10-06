60ea05839228440b3f29ea15d633f2f811df4118
AdrianoSuares <61994392+AdrianoSuares@users.noreply.github.com>
2020-10-05 21:50:34 -0300

    Add files via upload

---
+++ HTML/main.js
@@ -0,0 +1,12 @@
+
+function soma(){
+  var a = parseInt(document.getElementById('a').value);
+  var b = parseInt(document.getElementById('b').value);
+  var total = document.getElementById('total');
+
+  var total;
+
+  total = a + b;
+
+  total.innerHTML =total;
+}
